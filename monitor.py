3415cd502aa7f58abccb65095197aceac29b4353
Alexandre S. <alexandre.studenny@gmail.com>
2025-06-05 11:04:59 +0200

    monitor.py v-1.0.0

--- monitor.py
+++ monitor.py
@@ -1,4 +1,27 @@
 import requests
-import dotenv
+from dotenv import load_dotenv
 import os
 
+
+load_dotenv()
+
+# Récupération des variables url et key via le fichier .env
+url = os.getenv('URL')
+key = os.getenv('API_KEY')
+
+
+# Préparation de l'entête de la clé API
+headers = {
+    "Authorization": f"Bearer {key}"
+}
+
+# Préparation de la réponse du serveur 
+response = requests.get(url,headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+
+    print(data)
+
+else:
+    print('Erreur de connexion')
