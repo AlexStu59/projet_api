576bd03a924f9c687d3a36ccb0e7979da1052ced
Alexandre S. <alexandre.studenny@gmail.com>
2025-06-05 11:53:09 +0200

    monitor.py v-1.0.1 - Ajout des logs

--- monitor.py
+++ monitor.py
@@ -1,13 +1,35 @@
 import requests
 from dotenv import load_dotenv
 import os
+import logging
 
 
+
+# Configuration du logging
+logging.basicConfig(
+    level=logging.INFO,  # Niveau de log (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='app.log',  # Fichier de log
+    filemode='a'         # 'a' pour ajouter, 'w' pour écraser à chaque exécution
+)
+
+
+
+# Récupperation des variables d'environnement
 load_dotenv()
 
-# Récupération des variables url et key via le fichier .env
 url = os.getenv('URL')
 key = os.getenv('API_KEY')
+
+
+
+
+# Vérification des variables
+if not url or not key:
+    logging.error("URL ou API_KEY manquante dans le fichier .env")
+    exit(1)
+
+
 
 
 # Préparation de l'entête de la clé API
@@ -15,13 +37,29 @@ headers = {
     "Authorization": f"Bearer {key}"
 }
 
-# Préparation de la réponse du serveur 
-response = requests.get(url,headers=headers)
 
-if response.status_code == 200:
+
+
+# Envoi de la requête au serveur API
+try:
+    logging.info(f"Envoi de la requête GET au serveur {url}")
+    response = requests.get(url,headers=headers)
+    response.raise_for_status()  # Lève une exception pour les codes d'erreur HTTP
+
+    response.status_code == 200
     data = response.json()
+    logging.info("Réponse reçue avec succès")
 
     print(data)
 
-else:
-    print('Erreur de connexion')
+except requests.exceptions.HTTPError as http_err: # Erreur de type http
+    logging.error(f"Erreur http : {http_err}")
+    print(f"Erreur http : {http_err}")
+
+except requests.exceptions.RequestException as err:
+    logging.error(f'Erreur de connexion : {err}')
+    print(f'Erreur de connexion : {err}')
+
+except Exception as e:
+    logging.critical(f'Erreur inconnue : {e}')
+    print(f'Erreur inconnue : {e}')
